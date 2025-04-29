81a74288bd2b76e6cbcd874e91dcedcdf08fb137
K. Papadopoulos <kostashope@hotmail.com>
2025-04-29 18:37:45 +0300

    feat: datatable template added, sortSign function added

--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
+import { reverse, sortBy } from 'lodash-es';
 
 @Component({
   selector: 'app-simple-datatable',
@@ -7,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrl: './simple-datatable.component.css'
 })
 export class SimpleDatatableComponent {
+  @Input() data: EPerson[] | undefined;
 
+  sortOrder = {
+    givenName: 'none',
+    surName: 'none',
+    age:'none',
+    email:'none',
+    education:'none'
+  }
+
+  sortData(sortKey: keyof EPerson): void {
+    console.log(sortKey);
+    if (this.sortOrder[sortKey]==='asc') {
+      this.sortOrder[sortKey] = 'desc'
+      this.data = sortBy(this.data, sortKey).reverse();
+    } else {
+      this.sortOrder[sortKey] = 'asc';
+      this.data = sortBy(this.data, sortKey);
+    }
+    
+
+    for (let key in this.sortOrder) {
+      if (key !== sortKey) {
+        this.sortOrder[key as keyof EPerson] = 'none'
+      }
+    }
+    console.log(this.sortOrder);
+  }
+
+  sortSign(sortKey: keyof EPerson): string {
+    if (this.sortOrder[sortKey] === 'asc') return '\u2191'
+    else if (this.sortOrder[sortKey] === 'desc') return '\u2193'
+    else return '';
+  }
+
+  onPersonClicked(person:EPerson){
+    console.log("Person>>",person)
+  }
 }
